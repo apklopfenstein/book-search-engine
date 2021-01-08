1def1cac71ae52d74d6248432cf538a1f092b8d2
Alyssa Klopfenstein <alyssa.r.purdy@gmail.com>
2021-01-08 10:04:17 -0600

    clean up server side code

--- server/server.js
+++ server/server.js
@@ -1,15 +1,13 @@
-const path = require('path');
-
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-
 const { authMiddleware } = require('./utils/auth');
-
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-
+const path = require('path');
 const app = express();
+
 const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
